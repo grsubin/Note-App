fdd31dce18ca1c39c1feff6eebc3bbef3f91f164
grsubin <963subin@gmail.com>
2022-08-04 17:18:10 +0545

    changed note_body to body

--- app/controllers/notes.controller.js
+++ app/controllers/notes.controller.js
@@ -34,7 +34,7 @@ const createNote = async (req, res, next) => {
     guid = !guid ? uuidv4() : guid;
 
     const dbNote = await pool.query(
-      "INSERT INTO note (title, note_body, created_at, guid, user_id) VALUES ($1, $2, NOW(), $3, $4) RETURNING *",
+      "INSERT INTO note (title, body, created_at, guid, user_id) VALUES ($1, $2, NOW(), $3, $4) RETURNING *",
       [title, body, guid, userId]
     );
     const note = dbNote.rows[0];
@@ -42,7 +42,7 @@ const createNote = async (req, res, next) => {
     res.status(201).send(note);
   } catch (error) {
     console.log(error.message);
-    res.status(!error.code ? 500 : error.code).send({
+    res.status(error.code ? 500 : error.code).send({
       message: error.message,
     });
   }
@@ -68,7 +68,7 @@ const findNoteById = async (req, res, next) => {
     res.status(200).send(note);
   } catch (error) {
     console.log(error.message);
-    res.status(!error.code ? 500 : error.code).send({
+    res.status(error.code ? 500 : error.code).send({
       message: error.message,
     });
   }
@@ -83,7 +83,7 @@ const updateNoteById = async (req, res, next) => {
 
     const dbNote = (
       await pool.query(
-        "SELECT title, note_body FROM note WHERE user_id = $1 AND id = $2 AND deleted_at IS NULL ",
+        "SELECT title, body FROM note WHERE user_id = $1 AND id = $2 AND deleted_at IS NULL ",
         [userId, noteId]
       )
     ).rows[0];
@@ -95,19 +95,19 @@ const updateNoteById = async (req, res, next) => {
     }
 
     dbNote.title = !note.title ? dbNote.title : note.title;
-    dbNote.note_body = !note.body ? dbNote.note_body : note.body;
+    dbNote.body = !note.body ? dbNote.body : note.body;
 
     const updatedNote = (
       await pool.query(
-        "UPDATE note SET updated_at = NOW(), title = $1, note_body = $2 WHERE user_id = $3 AND id =$4 RETURNING *",
-        [dbNote.title, dbNote.note_body, userId, noteId]
+        "UPDATE note SET updated_at = NOW(), title = $1, body = $2 WHERE user_id = $3 AND id =$4 RETURNING *",
+        [dbNote.title, dbNote.body, userId, noteId]
       )
     ).rows[0];
 
     res.status(200).send(updatedNote);
   } catch (error) {
     console.log(error.message);
-    res.status(!error.code ? 500 : error.code).send({
+    res.status(error.code ? 500 : error.code).send({
       message: error.message,
     });
   }
